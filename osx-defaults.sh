4cfb17c8594057e4ce3f0a17f5d7017924aafd1d
Gunnar Hellekson <gunnar@hellekson.com>
2014-09-19 10:26:57 -0500

    added link to config page in Chrome

--- osx-defaults.sh
+++ osx-defaults.sh
@@ -1,4 +1,4 @@
-# Enable kerberos in Chrome
+# Enable kerberos in Chrome, verify in chrome://policy/
 defaults write com.google.Chrome AuthServerWhitelist "*.redhat.com"
 defaults write com.google.Chrome AuthNegotiateDelegateWhitelist "*.redhat.com"
 
