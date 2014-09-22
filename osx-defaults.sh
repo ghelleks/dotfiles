6921fd7ed4fcd070cc657cbeb2eccc0ba9fdae55
Gunnar Hellekson <gunnar@hellekson.com>
2014-09-21 20:12:41 -0500

    prefer plain text in emails

--- osx-defaults.sh
+++ osx-defaults.sh
@@ -2,3 +2,6 @@
 defaults write com.google.Chrome AuthServerWhitelist "*.redhat.com"
 defaults write com.google.Chrome AuthNegotiateDelegateWhitelist "*.redhat.com"
 
+# View Mail.app emails in plain text, like god intended
+defaults write com.apple.mail PreferPlainText -bool true
+
