dd45c1a2a46bfbccf8140c8051ad3cc3ddebf72f
Gunnar Hellekson <ghelleks@ghelleks.iad.csb>
2015-07-14 20:37:41 -0400

    added vim bootstrap script

---
+++ install-vim-goodies.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-fugitive.git
+git clone git://github.com/altercation/vim-colors-solarized.git
