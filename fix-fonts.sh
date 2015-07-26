e0733acbe279aadc401bbef60932b13d4f1cb5b0
Gunnar Hellekson <gunnar@hellekson.com>
2015-07-26 15:06:57 -0500

    Revise the whole font situation

--- fix-fonts.sh
+++ fix-fonts.sh
@@ -1,4 +1,16 @@
-sudo yum install freetype-freeworld
+# set up EL repo
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+
+# install Nux desktop
+rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+
+# install the infinality patches
+yum --enablerepo=nux-dextop install fontconfig-infinality
+yum --enablerepo=nux-dextop install cairo
+yum --enablerepo=nux-dextop install libXft
+yum --enablerepo=nux-dextop install freetype-infinality
+
 gsettings "set" "org.gnome.settings-daemon.plugins.xsettings" "hinting" "slight"
 gsettings "set" "org.gnome.settings-daemon.plugins.xsettings" "antialiasing" "rgba"
 echo "Xft.lcdfilter: lcddefault" > ~/.Xresources
